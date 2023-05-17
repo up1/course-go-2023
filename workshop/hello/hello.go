f255f40732103135a2eebd2017aead38fd6c1318
up1 <somkiat.p@gmail.com>
2023-05-17 10:56:36 +0700

    Demo with function

--- workshop/hello/hello.go
+++ workshop/hello/hello.go
@@ -1,5 +1,28 @@
 package hello
 
 func SayHello() {
-	println("Hello World")
+	i, err := doSth()
+	if err != nil {
+		println(err)
+	}
+	println("Hello World ", i)
+}
+
+func doSth() (int, error) {
+	return 1, nil
+}
+
+func tryLoop() {
+	for i := 0; i < 2; i++ {
+
+	}
+
+	i := 0
+	for i < 2 {
+		i++
+	}
+
+	for {
+
+	}
 }
