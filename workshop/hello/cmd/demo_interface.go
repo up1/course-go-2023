904e63b18d9287d3a0a00d3f8d4e507a664e7ddc
up1 <somkiat.p@gmail.com>
2023-05-17 14:28:33 +0700

    Demo with interface

--- workshop/hello/cmd/demo_interface.go
+++ workshop/hello/cmd/demo_interface.go
@@ -2,16 +2,30 @@ package main
 
 func main() {
 	v1 := V1{}
-	process(v1)
+	process1(v1)
+	process2(v1)
 }
 
-func process(i IDo) {
+func process1(i IDo) {
 	i.f1()
+}
+
+func process2(i interface{}) {
+	i.(IDo).f1()
+}
+
+func process3(i any) {
+	i.(IDo).f1()
+}
+
+type IDo2 interface {
+	f3() error
 }
 
 type IDo interface {
 	f1() error
 	f2() error
+	IDo2
 }
 type V1 struct{}
 
