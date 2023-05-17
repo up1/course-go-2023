316fbc6b460f85846b76bcf6b22c95bb7453fac9
up1 <somkiat.p@gmail.com>
2023-05-17 12:03:30 +0700

    Use testify

--- workshop/hello/hello_test.go
+++ workshop/hello/hello_test.go
@@ -3,6 +3,8 @@ package hello_test
 import (
 	"hello"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSayHi(t *testing.T) {
@@ -11,11 +13,12 @@ func TestSayHi(t *testing.T) {
 	if r != "Hello World 1" {
 		t.Errorf("Test fail ... got %s", r)
 	}
+	assert.Equal(t, "Hello World 1", r)
 }
 
 //go test
 //go test ./...
 //go test -v
 //go test -v -cover
-//go test -coverprofile=coverage.out
+//go test ./... -coverprofile=coverage.out
 //go tool cover -html=coverage.out
