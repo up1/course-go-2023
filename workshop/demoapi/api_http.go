190bca9722910e8f75087d748c7fe6c1c3ef9cf3
up1 <somkiat.p@gmail.com>
2023-05-17 16:20:33 +0700

    API workshop

---
+++ workshop/demoapi/api_http.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
